18ce1ed393114e2ff4e14603c3f83f7370fbea83
Allan Farca <allan.farca@gmail.com>
2020-04-22 07:38:10 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,19 @@
+class User 
+  
+  def first_name
+    @first_name
+  end
+ 
+  def first_name=(new_name)
+    @first_name = new_name
+  end
+  
+  def last_name
+    @last_name
+  end
+ 
+  def last_name=(new_name)
+    @last_name = new_name
+  end
+    
+end 
